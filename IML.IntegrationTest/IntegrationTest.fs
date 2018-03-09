ec3c188a14ed237c60b07de44630be204e4d079a
Will Johnson <william.c.johnson@intel.com>
2018-03-09 15:18:03 -0500

    Add 'remove device' integration test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- IML.IntegrationTest/IntegrationTest.fs
+++ IML.IntegrationTest/IntegrationTest.fs
@@ -4,13 +4,14 @@
 
 module IML.IntegrationTest.IntegrationTest
 
-open Fable.Import.Jest.Matchers
 open Fable.Core.JsInterop
 open Fable.PowerPack
 open IML.StatefulPromise.StatefulPromise
 open IML.IntegrationTestFramework.IntegrationTestFramework
 
+open Fable.Import
 open Fable.Import.Jest
+open Fable.Import.Jest.Matchers
 open Fable.Import.Node
 open Fable.Import.Node.PowerPack
 open Fable.PowerPack.Json
@@ -32,18 +33,41 @@ let resultOutput: StatefulResult<State, Out, Err> -> string = function
   | Ok ((Stdout(r), _), _) -> r
   | Error (e) -> failwithf "Command failed: %A" !!e
 
+let rbScanForDisk (hostId:string) (rollbackState:RollbackState): JS.Promise<RollbackStateResult<Out, Err>> =
+  let command = sprintf "echo \"- - -\" > /sys/class/scsi_host/host%s/scan" hostId
+  command
+    |> (execShell >> (addToRollbackState command rollbackState))
+
+let setDeviceState (name:string) (state:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"%s\" > /sys/block/%s/device/state" state name)
+
+let deleteDevice (name:string) (hostId:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"1\" > /sys/block/%s/device/delete" name)
+    >> rollback (rbScanForDisk hostId)
+
+let matchResultToSnapshot (r:StatefulResult<State, Out, Err>, _): unit =
+  let json =
+    r
+      |> resultOutput
+      |> unwrapDeviceData
+      |> toJson
+      |> buffer.Buffer.from
+
+  toMatchSnapshot json
+
 testAsync "info event" <| fun () ->
   command {
     return! scannerInfo
   }
   |> startCommand "Info Event"
-  |> Promise.map (fun (r, _) ->
-      let json =
-        r
-          |> resultOutput
-          |> unwrapDeviceData
-          |> toJson
-          |> buffer.Buffer.from
+  |> Promise.map matchResultToSnapshot
 
-      toMatchSnapshot json
-  )
+testAsync "remove a device" <| fun () ->
+  command {
+    do! (setDeviceState "sdc" "offline") >> ignoreCmd
+    do! (deleteDevice "sdc" "4") >> ignoreCmd
+    return! scannerInfo
+
+  }
+  |> startCommand "removing a device"
+  |> Promise.map matchResultToSnapshot
